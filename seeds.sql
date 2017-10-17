af706e8759b7d93a1f6283303aaa66a6824f5dbe
ruthieirl <ruthfromjersey@gmail.com>
2017-10-16 22:27:03 -0400

    seeds complete

---
+++ seeds.sql
@@ -0,0 +1,15 @@
+USE bamazon;
+
+INSERT INTO products 
+	(product_name, department_name, price, stock_quantity)
+VALUES
+	("Bounce Curl Curling Lotion", "health & beauty", 28.95, 35),	
+	("Kinky-Curly Original Curling Custard", "health & beauty", 18.15, 23),
+	("Curly Sexy Hair Curling Creme", "health & beauty", 14.80, 29),
+	("Miss Jessie's Pillow Soft Curls", "health & beauty", 23.00, 13),
+	("Cantu Shea Butter Leave-In Conditioner", "health & beauty", 21.99, 17),
+	("Ouidad Featherlight Styling Creme", "health & beauty", 26.00, 29),
+	("TiGi Catwalk Curls Rock Amplifier", "health & beauty", 18.29, 48),
+	("Renpure Cocnut Creme Curling Jelly Gel", "health & beauty", 9.49, 66),
+	("Devacurl No-Poo and One Conditioner Set", "health & beauty", 53.00, 57),
+	("Mixed Chicks Leave-In Conditioner", "health & beauty" 17.08, 44);
